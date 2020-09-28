572c785ef698ae85ce9ca398b97409d23e98daf1
constantineKyr <constantineKyr@gmail.com>
2020-09-28 15:28:54 +0300

    InstaLike: limits total likes to 20
    Signed-off-by: constantineKyr <constantineKyr@gmail.com>

--- InstaLike.py
+++ InstaLike.py
@@ -15,10 +15,10 @@ class InstaLike(InstaLogin):
             time.sleep(random.randint(1,2))
             self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div').click()
             time.sleep(random.randint(1,2))
-            for x in range(1,200):
+            for x in range(0,20):
                 self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button').click()
                 time.sleep(random.randint(2,5))
-                if x == 1:
+                if x == 0:
                     self.driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a').click()
                 else:
                     self.driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a[2]').click()
